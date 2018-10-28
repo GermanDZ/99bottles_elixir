946f877e541e571308799547f5ec87a562348a2b
GermanDZ <germ@ndz.com.ar>
2018-10-28 15:35:16 +0100

    solution for 99

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -1,1 +1,8 @@
-
+defmodule Bottles do
+  def verse(99) do
+    """
+    99 bottles of beer on the wall, 99 bottles of beer.
+    Take one down and pass it around, 98 bottles of beer on the wall.
+    """
+  end
+end
